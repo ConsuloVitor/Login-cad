94577bd70ae27c544c636fab1d740b9e533a0043
Vitor <vitor.consulo@alunoescolassociais.org.br>
2024-07-04 09:01:06 -0300

    Adicionando a 1º fuction e validando ela no html

---
+++ controller.js
@@ -0,0 +1,6 @@
+// VALIDAR ACESSO TELA DE LOGIN
+
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('logiSenha').value;
+}
