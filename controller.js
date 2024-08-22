357efea6502f627b8f138c554b4e209e9e3ea8ff
Vitor <reis.consulo@gmail.com>
2024-08-22 20:58:55 -0300

    detalhes

--- controller.js
+++ controller.js
@@ -16,39 +16,44 @@ function acessar(){
 // FUNÇÃO QUE ARMAZENA NOME NA TELA DE CADASTRO
 // O CÓDIGO ADICIONA UM NOME À LISTA E LIMPA O CAMPO SE O NOME NÃO ESTIVER VAZIO, CASO CONTRÁRIO, EXIBE UM ALERTA
 var dadosLista = [];
-function salvarUser(){
+function salvarUser() {
     let nomeUser = document.getElementById('nomeUser').value;
-
-    if(nomeUser){
-        dadosLista.push(nomeUser);
-        // console.log(dadosLista);
-        criaLista();
-        document.getElementById("nomeUser").value = "";
-    }else{
-        alert("Por favor informar o nome para cadastro");
+    let emailUser = document.getElementById('emailUser').value;
+ 
+    if (nomeUser && emailUser) {
+        dadosLista.push({ nome: nomeUser, email: emailUser });
+        criarLista();
+        document.getElementById('nomeUser').value = "";
+        document.getElementById('emailUser').value = ""; 
+    } else {
+        alert('Por favor, informe um nome e um e-mail para cadastro.');
     }
 }
 
 // FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
-// O CÓDIGO ATUALIZA O CONTEÚDO DA TABELA HTML PARA EXIBIR OS NOSMES DA LISTA, ELE CRIA LINHAS PARA CADA NOME, ADICIONANDO "BOTÕES DE EDITAR E EXCLUIR"
-function criaLista(){
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    for(let i=0;i<=(dadosLista.length - 1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-        document.getElementById('tabela').innerHTML = tabela;
+// O CÓDIGO ATUALIZA O CONTEÚDO DA TABELA HTML PARA EXIBIR OS NOMES DA LISTA, ELE CRIA LINHAS PARA CADA NOME, ADICIONANDO "BOTÕES DE EDITAR E EXCLUIR"
+function criarLista() {
+    let tabela = "<tr><th>Nome Usuario</th><th>Email</th><th>Ações</th></tr>";
+    for (let i = 0; i < dadosLista.length; i++) {
+        tabela += "<tr><td>" + dadosLista[i].nome + "</td><td>" + dadosLista[i].email + "</td>" +
+                  "<td><button type='button' onclick='editar(" + i + ")'>Editar</button>" +
+                  "<button type='button' onclick='excluir(" + i + ")'>Excluir</button></td></tr>";
     }
+    document.getElementById('tabela').innerHTML = tabela;
 }
-
+ 
 // FUNÇÃO PARA EDITAR NOMES DE LISTA
 // O CÓDIGO ATUALIZA O CAMPO `NOMEUSER` COM O VALOR DO ITEM NA POSIÇÃO 'I-1' DO ARRAY `DADOSLISTA` E, EM SEGUIDA, TENTA REMOVER UM ITEM DE DADOSLISTA BASEADO NO VALOR DO ITEM NA POSIÇÃO 'I-1', O QUE PODE NÃO FUNCIONAR CORRETAMENTE.
-function editar(i){
-    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1)], 1);
+function editar(i) {
+    document.getElementById('nomeUser').value = dadosLista[i].nome;
+    document.getElementById('EmailUser').value = dadosLista[i].email;
+    dadosLista.splice(i, 1); 
+   criarLista(); 
 }
-
+ 
 // FUNÇÃO PARA EXCLUIR NOME DE LISTA
 // A FUNÇÃO EXCLUIR REMOVE O ITEM DO ARRAY E EXCLUI A LINHA CORRESPONDENTE DA TABELA HTML COM O ID TABELA.
-function excluir(i){
-    dadosLista.splice((i - 1), 1);
-    document.getElementById('tabela').deleteRow(i);
+function excluir(i) {
+    dadosLista.splice(i, 1);
+    criarLista(); 
 }
