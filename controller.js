ab4eccf4ebae9cc1aaf11e3d4abea8f3e3941b95
Vitor <reis.consulo@gmail.com>
2024-08-28 14:27:04 -0300

    validando o cpf

--- controller.js
+++ controller.js
@@ -5,6 +5,40 @@ function validarEmail(email) {
     return padraoEmail.test(email);
 }
 
+// FUNÇÃO QUE VALIDA O FORMATO DO CPF
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g,''); // Remove caracteres não numéricos
+
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false; // Verifica se tem 11 dígitos e se todos os dígitos são iguais
+    }
+
+    let soma = 0;
+    let resto;
+
+    // Validação do primeiro dígito verificador
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if ((resto === 10) || (resto === 11)) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+    soma = 0;
+
+    // Validação do segundo dígito verificador
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if ((resto === 10) || (resto === 11)) resto = 0;
+    if (resto !== parseInt(cpf.substring(10, 11))) return false;
+
+    return true;
+}
+
 // VALIDAR ACESSO TELA DE LOGIN
 // VALIDA SE OS CAMPOS FORAM PREENCHIDO E O REDIRECIONA PARA PAGINA CADASTRO, CASO AO CONTRARIO ENVIA UM ALERTA
 function acessar(){
